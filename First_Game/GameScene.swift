7f4d113fe0452e6e55a565f4f5c980989c559bb8
Roy Alameh <roy.alameh24@d155.org>
2022-09-10 13:28:15 -0500

    Added torpedo and explosion when hitting an alien

--- First_Game/GameScene.swift
+++ First_Game/GameScene.swift
@@ -19,6 +19,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         }
     }
     var gameTimer:Timer!
+    var alienCategory:UInt32 = 1
+    var torpedoCategory:UInt32 = 2
+    
+    
     
     override func didMove(to view: SKView) {
         starfield = SKEmitterNode(fileNamed: "Space")
@@ -49,7 +53,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         //does not work
         /*gameTimer = Timer(timeInterval: 0.5, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true) */
         
-        run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 0.5), SKAction.run(addAlien)])))
+        run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 0.75), SKAction.run(addAlien)])))
         
         let animationDuration:TimeInterval = 3
         let animation1:SKAction = SKAction.move(to: CGPoint(x: 768 - player.size.width / 2, y: player.size.height / 2), duration: animationDuration)
@@ -75,6 +79,49 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         actions.append(SKAction.move(to: CGPoint(x: alien.position.x, y: -alien.size.height / 2), duration: 5))
         actions.append(SKAction.run(alien.removeFromParent))
         alien.run(SKAction.sequence(actions))
+        
+        alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
+        alien.physicsBody?.isDynamic = false
+        alien.physicsBody?.affectedByGravity = false
+        
+        alien.physicsBody?.categoryBitMask = alienCategory
+        alien.physicsBody?.contactTestBitMask = torpedoCategory
+        alien.physicsBody?.collisionBitMask = 0
+    }
+    
+    func fireTorpedo() {
+        let torpedo = SKShapeNode(circleOfRadius: 5)
+        torpedo.fillColor = UIColor.green
+        torpedo.position = CGPoint(x: player.position.x, y: player.position.y)
+        let finalPosition = CGPoint(x: torpedo.position.x, y: 1300)
+        torpedo.run(SKAction.move(to: finalPosition, duration: 1.25))
+        self.addChild(torpedo)
+        
+        torpedo.physicsBody = SKPhysicsBody(circleOfRadius: 5)
+        torpedo.physicsBody?.categoryBitMask = torpedoCategory
+        torpedo.physicsBody?.contactTestBitMask = alienCategory
+        torpedo.physicsBody?.collisionBitMask = 0
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.categoryBitMask == 1 && contact.bodyB.categoryBitMask == 2 {
+            alienHit(alien: contact.bodyA.node as! SKSpriteNode, torpedo: contact.bodyB.node as! SKShapeNode)
+        }
+        else if contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 1 {
+            alienHit(alien: contact.bodyB.node as! SKSpriteNode, torpedo: contact.bodyA.node as! SKShapeNode)
+        }
+    }
+    
+    func alienHit(alien:SKSpriteNode, torpedo:SKShapeNode) {
+        let explosion = SKEmitterNode(fileNamed: "explosion")!
+        explosion.position = alien.position
+        self.addChild(explosion)
+        alien.removeFromParent()
+        torpedo.removeFromParent()
+        
+        score += 1
+        
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -82,25 +129,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            //makes explosion appear where tapped, not dragged
-            explosion = SKEmitterNode(fileNamed: "explosion")
-            explosion.position = CGPoint(x: location.x, y: location.y)
-            self.addChild(explosion)
-            score += 1
-            
-        }
+        fireTorpedo()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.location(in: self)
-            //allows you to drag spaceship with finger
-            //player.position = CGPoint(x: location.x, y: location.y)
-            explosion = SKEmitterNode(fileNamed: "explosion")
-            explosion.position = CGPoint(x: location.x, y: location.y)
-            self.addChild(explosion)
+            
             
         }
     }
