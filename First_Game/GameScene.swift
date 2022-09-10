3522225982dee60dc28320889a4ab4e81efac60b
Roy Alameh <roy.alameh24@d155.org>
2022-09-10 08:24:52 -0500

    Added enemies and score label

--- First_Game/GameScene.swift
+++ First_Game/GameScene.swift
@@ -12,6 +12,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     var starfield:SKEmitterNode!
     var explosion:SKEmitterNode!
     var player:SKSpriteNode!
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var gameTimer:Timer!
     
     override func didMove(to view: SKView) {
         starfield = SKEmitterNode(fileNamed: "Space")
@@ -31,17 +38,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         player.position = CGPoint(x: self.frame.size.width / 2, y : player.size.height / 2 + 20)
         self.addChild(player)
         
-        //self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-        //self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 100, y: self.frame.size.height - 60)
+        self.addChild(scoreLabel)
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        
+        //does not work
+        /*gameTimer = Timer(timeInterval: 0.5, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true) */
+        
+        run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 0.5), SKAction.run(addAlien)])))
         
         let animationDuration:TimeInterval = 3
         let animation1:SKAction = SKAction.move(to: CGPoint(x: 768 - player.size.width / 2, y: player.size.height / 2), duration: animationDuration)
         let animation2:SKAction = SKAction.move(to: CGPoint(x: 0 + player.size.width / 2, y: player.size.height / 2), duration: animationDuration)
         
-        var actionArray = [animation1, animation2]
+        let actionArray = [animation1, animation2]
         
         player.run(SKAction.repeatForever(SKAction.sequence(actionArray)))
         
+    }
+    
+    func addAlien() {
+        let alien = SKSpriteNode(imageNamed: "rocket2")
+        alien.zRotation = Double.pi
+        alien.size.width = alien.size.width / 8
+        alien.size.height = alien.size.height / 8
+        
+        let xPosition = CGFloat(GKRandomDistribution(lowestValue: Int(alien.size.width) / 2, highestValue: 768 - Int(alien.size.width / 2)).nextInt())
+        alien.position = CGPoint(x: xPosition, y: 1024 + alien.size.height / 2)
+        self.addChild(alien)
+        
+        var actions = [SKAction]()
+        actions.append(SKAction.move(to: CGPoint(x: alien.position.x, y: -alien.size.height / 2), duration: 5))
+        actions.append(SKAction.run(alien.removeFromParent))
+        alien.run(SKAction.sequence(actions))
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -55,6 +88,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             explosion = SKEmitterNode(fileNamed: "explosion")
             explosion.position = CGPoint(x: location.x, y: location.y)
             self.addChild(explosion)
+            score += 1
             
         }
     }
